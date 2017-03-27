cf46839cf9e40852ae6b19f163e8eb68e098ef21
Richard Wiz <richard.wisbey@tattsgroup.com>
2017-03-27 15:43:56 +1000

    Commit Toqueo

---
+++ Torqueo/NextGen Data Objects/SpinJSON.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Torqueo
+{
+    /// <summary>
+    /// Maps to NextGen.dbo.SpinJson, thus;
+    /// USE [NextGen]
+    /// CREATE TABLE[dbo].[SpinJson](
+    /// [Id] [int] IDENTITY(1,1) NOT NULL,
+    /// [Sport] [nvarchar](50) NOT NULL,
+    /// [Imported] [datetime]
+    /// </summary>
+    public class SpinJSON
+    {
+        public int JsonId { get; set; }
+        public String Sport { get; set; }
+        public DateTime Imported { get; set; }
+
+        public SpinJSON()
+        {
+            this.JsonId = 0;
+            this.Sport = String.Empty;
+            this.Imported = DateTime.Now;
+        }
+    }
+}
