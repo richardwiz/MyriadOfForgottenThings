893550c0e95f18e4499a0afe35662aba87382851
Richard Wiz <richardwisbey@yahoo.co.uk>
2018-04-03 10:46:10 +1000

    Commit

---
+++ Argonaut/Program.cs
@@ -0,0 +1,56 @@
+﻿using JaSON;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema;
+using PapaLegba;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Argonaut
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         /*// load from a string
+         JsonSchema schema1 = JsonSchema.Parse(@"{'type':'object'}");
+
+         // load from a file
+         using (TextReader reader = File.OpenText(@"c:\schema\Person.json"))
+         {
+             JsonSchema schema2 = JsonSchema.Read(new JsonTextReader(reader));
+
+             // do stuff
+         }*/
+
+         Jedi luke = new Jedi(100, "Luke Skywalker");
+         AMQMessage amq = new AMQMessage();
+
+         String json = JsonConvert.SerializeObject(luke);       
+         Console.WriteLine("\nJaSON the jedi is; \n {0}\n", json);
+         JObject jo = JObject.Parse(json);
+         JsonSchema js = new JsonSchema();
+         js.Type = JsonSchemaType.Object;
+         js.Properties = new Dictionary<string, JsonSchema>
+         {
+             { "Name", new JsonSchema { Type = JsonSchemaType.String, Required = true} },
+             { "Force", new JsonSchema {Type = JsonSchemaType.Integer , Required = true} }
+         };
+         Console.WriteLine("\nJaSON's schema is; \n {0}\n", js.ToString());
+
+         String isValid = JaSONTools.ValidateJSON(jo, js) ? String.Format("{0} is a Valid", jo.ToString()) : String.Format("{0} is NOT Valid", jo.ToString());
+         Console.WriteLine("{0}", isValid);
+
+         json = JsonConvert.SerializeObject(amq);
+         Console.WriteLine("\nJaSON the amq is; \n {0}\n", json);
+         jo = JObject.Parse(json);
+         isValid = JaSONTools.ValidateJSON(jo, js) ? String.Format("{0} is a Valid", jo.ToString()) : String.Format("{0} is NOT Valid", jo.ToString());
+
+         Console.WriteLine("{0}", isValid);
+         Console.Read();
+      }
+   }
+}
---
+++ JaSON/JaSONTools.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JaSON
+{
+    public static class JaSONTools
+    {
+      public static Boolean ValidateJSON(JObject jo, JsonSchema js )
+      {
+  
+         return jo.IsValid(js);
+
+      }
+   }
+}
